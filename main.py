d53d1732b677141596bb54c1a981c7ffebf0a997
Rui-Zhang1997 <r.zhang194@gmail.com>
2018-01-13 23:50:36 -0500

    home almost there

--- main.py
+++ main.py
@@ -29,6 +29,12 @@ class NavbarItemCollection:
     def get_tabs(self):
         return [self.items[name] for name in self.item_order]
 
+class Skill:
+    def __init__(self, name, text, rating):
+        self.name = name
+        self.text = text
+        self.rating = rating
+
 navbar_items = [
     NavbarItem("home", "Home"),
     NavbarItem("projects", "Projects"),
@@ -36,10 +42,30 @@ navbar_items = [
 ]
 
 navbar_collection = NavbarItemCollection(navbar_items)
+prog_skills = [
+    Skill('python', "Python", 5),
+    Skill("javascript", "JavaScript", 5),
+    Skill("java", "Java", 5),
+    Skill('kotlin', 'Kotlin', 4),
+    Skill('c', "C", 4),
+    Skill('cpp', 'C++', 3),
+    Skill('sql', 'SQL', 4)
+]
+
+framework_skills = [
+    Skill("flask", "Flask", 5),
+    Skill("rxjava", "RxJava", 5),
+    Skill("android", "Android", 5),
+    Skill("nodejs", "Node.js", 4),
+    Skill("expressjs", "Express.js", 4),
+    Skill("mysql", "MySQL/MariaDB", 5),
+    Skill("nginx", "Nginx", 3),
+    Skill("docker", "Docker", 3)
+]
 
 @app.route("/")
 def main():
     navbar_collection.set_active("home")
-    return render_template("main.html", navbarItems=navbar_collection.get_tabs())
+    return render_template("main.html", navbarItems=navbar_collection.get_tabs(), progSkills=prog_skills+framework_skills)
 
 app.run(debug=True, port=3000)
---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+scss_dir="./static/scss"
+css_dir="./static/css"
+ext=".css"
+for file in $scss_dir/*
+do
+    fname="${file%.*}"
+    echo "scss $file $css_dir/$(basename $fname)$ext"
+    scss $file $css_dir/$(basename $fname)$ext
+done
+python main.py
