68c0cc8375097cef207570f1d39f298a2403db00
Rui <rz187@scarletmail.rutgers.edu>
2018-01-17 11:06:19 -0500

    Adds links

--- static/js/base.js
+++ static/js/base.js
@@ -10,4 +10,11 @@ $(function () {
             window.location.href = $(this).attr('uri');
         }
     });
+
+    $('.link-nt').click(function () {
+        if (!$(this).attr('class').includes('deactivate')) {
+            let win = window.open($(this).attr('uri'), '_blank');
+            win.focus();
+        }
+    });
 });
