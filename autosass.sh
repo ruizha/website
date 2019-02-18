00aa59f78fa30054029404721db49236f0f5d518
Rui Zhang <r.zhang194@gmail.com>
2019-02-17 20:46:39 -0500

    Adds css minification

--- autosass.sh
+++ autosass.sh
@@ -7,5 +7,5 @@ for file in $scss_dir/*
 do
     fname="${file%.*}"
     echo "sass $file $css_dir/$(basename $fname)$ext"
-    sass $file $css_dir/$(basename $fname)$ext
+    sass $file $css_dir/$(basename $fname)$ext --style compressed
 done
